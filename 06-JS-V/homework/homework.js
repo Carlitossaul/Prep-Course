4640506f1ef392493d12784b857eca434ab4cde0
Carlitossaul <Carlitossaul.h@gmail.com>
2022-08-04 23:05:43 -0300

    Finish Homework

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -9,15 +9,27 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
-function Usuario (opciones){
-  this.usuario = opciones.usuario,
-  this.nombre = opciones.nombre,
-  this.email = opciones.email,
-  this.password = opciones.password
-}
-Usuario.prototype.saludar = function(){
-  return 'Hola, mi nombre es '+ this.nombre
-}
+  // function Usuario (opciones){
+  //   this.usuario = opciones.usuario,
+  //   this.nombre = opciones.nombre,
+  //   this.email = opciones.email,
+  //   this.password = opciones.password
+  // }
+  // Usuario.prototype.saludar = function(){
+  //   return 'Hola, mi nombre es '+ this.nombre
+  // }
+  // return Usuario
+  class Usuario {
+    constructor(opciones) {
+      this.usuario = opciones.usuario,
+        this.nombre = opciones.nombre,
+        this.email = opciones.email,
+        this.password = opciones.password
+    }
+    saludar() {
+      return 'Hola, mi nombre es ' + this.nombre
+    }
+  }
 return Usuario
 }
 
